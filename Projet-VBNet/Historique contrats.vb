b0a11fe60656f8004563e00d883b6f74239e27d1
Sdiall <72425955+Sdiall@users.noreply.github.com>
2021-03-13 18:49:32 -0800

    Classe historique contrat terminé

--- Projet-VBNet/Historique contrats.vb
+++ Projet-VBNet/Historique contrats.vb
@@ -1,3 +1,25 @@
 ﻿Public Class Historique_contrats
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Historique_contrats_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
 End Class
